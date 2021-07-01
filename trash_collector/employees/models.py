afedc4fb64b0ea3e413e07b3a07e060008ababc2
carter-willey <cjwilley23@gmail.com>
2021-07-01 14:27:59 -0500

    Employee Create view is functional

--- trash_collector/employees/models.py
+++ trash_collector/employees/models.py
@@ -3,5 +3,8 @@ from django.db import models
 
 # Create your models here.
 class Employees(models.Model):
-    pass
+    name = models.CharField(max_length=50, default="")
+    user = models.ForeignKey('accounts.User', blank=True, null=True, on_delete=models.PROTECT)
+    zip_code = models.IntegerField(default=00000, null=True)
+
 # TODO: Create an Employee model with properties required by the user stories
--- trash_collector/employees/views.py
+++ trash_collector/employees/views.py
@@ -1,13 +1,45 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render, redirect
 from django.apps import apps
 
 # Create your views here.
 
 # TODO: Create a function for each path created in employees/urls.py. Each will need a template as well.
+from django.urls import reverse
+
+from .models import Employees
 
 
 def index(request):
     # This line will get the Customer model from the other app, it can now be used to query the db
     Customer = apps.get_model('customers.Customer')
-    return render(request, 'employees/index.html')
+    user = request.user
+    if not Employees.objects.filter(user_id=user.id).exists():
+        #  If user isn't in current Employee database, then create an employee with user information.
+        return redirect('create/')
+    else:
+        # Go into the home portal with user information found in Employee database.
+        specific_employee = Employees.objects.get(user_id=user.id)
+        all_customers = Customer.objects.all()
+        context = {
+            'user': user,
+            'all_customers': all_customers,
+            'specific_employee': specific_employee
+        }
+        print(user)
+        return render(request, 'employees/index.html', context)
+
+
+def create(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        zip_code = request.POST.get('zip_code')
+        new_employee = Employees(
+            user_id=request.user.id,
+            name=name,
+            zip_code=zip_code,
+        )
+        new_employee.save()
+        return HttpResponseRedirect(reverse('employees:index'))
+    else:
+        return render(request, 'employees/create.html')
